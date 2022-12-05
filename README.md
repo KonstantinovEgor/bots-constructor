d5cefb8f4605909dcfd106b0106888787002ce46
ErnestRaevskij <egaega802@gmail.com>
2022-12-05 12:02:42 +0300

    e.konstantinov
    Initial update

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# bots-constructor
+# Service for construction simple bots
+
+## Table of content
+- [info](#info)
+
+## Info
+- Node version 18.7.0
+- NPM version 8.15.0
+- PostgreSQL version 12.x.x
+- Support REST API architecture
+- Support data base migrations
+- Telegram bot API version 6.3
---
+++ config/index.js
@@ -0,0 +1,15 @@
+const publicRoutes = require('./routes/publicRoutes');
+const privateRoutes = require('./routes/privateRoutes');
+
+require('dotenv')
+    .config();
+
+const config = {
+        port: process.env.PORT || 5000,
+        host: process.env.HOST || 'localhost',
+        environment: process.env.NODE_ENV || 'development',
+        publicRoutes,
+        privateRoutes
+}
+
+module.exports = config;
