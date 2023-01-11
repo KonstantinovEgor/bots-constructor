c5c1fad1d45a9df6271416deae9b43d283f230d7
ErnestRaevskij <egaega802@gmail.com>
2023-01-11 12:31:19 +0300

    e.konstantinov
    init

--- server/api/services/Server.js
+++ server/api/services/Server.js
@@ -1,6 +1,7 @@
 const http = require('http');
-const chalk = require();
+const chalk = require('chalk');
 
+const database = require('../../config/database');
 const config = require('../../config/');
 
 class Server {
@@ -10,16 +11,33 @@ class Server {
         this.host = config.host;
     }
 
+    connect() {
+        database
+        .authenticate()
+        .then(() => {
+            console.log(
+                chalk.green('Database connection has been established successfully')
+            );   
+        })
+        .catch(error => {
+            console.log(
+                chalk.red(`Fail connecting to database:\n${error}`)
+            );
+        });
+    }
+
     start() {
         try {
             this.server.listen(this.port, () => {
                 console.log(
                     chalk.green(`\nServer success started`)
                 );
+
+                this.connect();
             });
         } catch (error) {
             console.log(
-                chalk.red(`\nFail to start Server:\n${error}`)
+                chalk.red(`Fail to start Server:\n${error}`)
             );
         }
     }
